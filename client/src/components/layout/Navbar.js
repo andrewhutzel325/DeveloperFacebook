826abbfcec8db291f941401bda7245817fbb2dcf
Andrew H <ahutzel@mail.sfsu.edu>
2019-11-11 23:03:10 -0800

    Removing modules on client

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
-        <Link to="/profiles">Developers</Link>
+        <Link to="/profiles">List of Current Developers</Link>
       </li>
       <li>
         <Link to="/posts">Posts</Link>
@@ -31,7 +31,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <Link to="/profiles">Developers</Link>
+        <Link to="/profiles">List of Current Developers</Link>
       </li>
       <li>
         <Link to="/register">Register</Link>
@@ -46,7 +46,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <nav className="navbar bg-dark">
       <h1>
         <Link to="/">
-          <i className="fas fa-code"></i> DevConnector
+          <i className="fas fa-code"></i> Developer Connector
         </Link>
       </h1>
       {!loading && (
@@ -65,7 +65,4 @@ const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(
-  mapStateToProps,
-  { logout }
-)(Navbar);
+export default connect(mapStateToProps, { logout })(Navbar);
